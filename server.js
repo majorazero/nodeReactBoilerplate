79d8f4fbbe70d8a573700a1c3a29a9b0862122a3
Dan Hsu <daniel.cw.hsu@gmail.com>
2019-12-15 23:25:03 -0800

    added express as dependency and added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path    = require('path');
+const PORT    = process.env.PORT || 3000;
+const app     = express();
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
